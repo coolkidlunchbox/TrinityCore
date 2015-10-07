f7ac03a8e51e15130af984d5506ed23c2197ff36
CoolKidLunchBox <pnodarte7@gmail.com>
2015-10-07 14:21:52 -0700

    Updated Account Management
    Load Time Increase.

--- src/server/game/Accounts/AccountMgr.cpp
+++ src/server/game/Accounts/AccountMgr.cpp
@@ -14,6 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Load time increase by CKLB
  */
 
 #include "AccountMgr.h"
